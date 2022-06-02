89ab205eeed8540605a7c39437489cebf5f693b8
KotaSHIOZAKI <ksw2270274@stu.o-hara.ac.jp>
2022-06-02 05:47:53 +0000

    config.jsonの修正

--- db/pool.js
+++ db/pool.js
@@ -7,6 +7,7 @@ const pool = new Pool({
     port: 5432,
     user: 'pkhhfjbkjcjyjy',
     password: 'd4a65cb046c99cd0af587aec46743c4a680ffb42b254e0fe4ee51e7f1bfe8cdd',
+    connectionString: process.env.DATABASE_URL,
     ssl: { 
         sslmode: 'require',
         rejectUnauthorized: false
