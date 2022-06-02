35092ad85af985be39ed851d5be95dbccbc72c3f
KotaSHIOZAKI <ksw2270274@stu.o-hara.ac.jp>
2022-06-02 05:37:03 +0000

    プールの追加

---
+++ db/pool.js
@@ -0,0 +1,16 @@
+const { Pool } = require("pg");
+
+// DB情報をもったプールを生成
+const pool = new Pool({
+    host: 'ec2-54-211-255-161.compute-1.amazonaws.com',
+    database: 'db2qtc7fdrjjvu',
+    port: 5432,
+    user: 'pkhhfjbkjcjyjy',
+    password: 'd4a65cb046c99cd0af587aec46743c4a680ffb42b254e0fe4ee51e7f1bfe8cdd',
+    ssl: { 
+        sslmode: 'require',
+        rejectUnauthorized: false
+    }
+});
+
+module.exports = pool;
